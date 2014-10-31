f9d98360a654839240bc93d9cb1d0c2a9f9d5feb
bennyk <benny_khoo_99@yahoo.com>
2014-10-31 12:55:12 +0800

    added initial FIFO read code but have to disable due to frequent buffer overflow.

--- libraries/AP_InertialSensor/AP_InertialSensor_MPU60X0.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor_MPU60X0.cpp
@@ -15,6 +15,8 @@ extern const AP_HAL::HAL& hal;
 #define MPU9150_ACCEL_SCALE_4G    (GRAVITY_MSS / 8192.0f)
 #define MPU9150_ACCEL_SCALE_2G    (GRAVITY_MSS / 16384.0f)
 
+//#define MPU60X0_ENABLE_FIFO
+
 AP_InertialSensor_MPU60X0::AP_InertialSensor_MPU60X0() :
     AP_InertialSensor(),
     _accel_filter_x(800, 10),
@@ -78,6 +80,30 @@ uint16_t AP_InertialSensor_MPU60X0::_init_sensor( Sample_rate sample_rate )
     mpu_set_lpf(_default_filter_hz);
     mpu_set_sample_rate(800);
 
+#ifdef MPU60X0_ENABLE_FIFO
+    _accelgyro.setFIFOEnabled(false);
+    _accelgyro.resetFIFO();
+    hal.scheduler->delay(100);
+
+    _accelgyro.setTempFIFOEnabled(false);
+    _accelgyro.setSlave0FIFOEnabled(false);
+    _accelgyro.setSlave1FIFOEnabled(false);
+    _accelgyro.setSlave2FIFOEnabled(false);
+    _accelgyro.setSlave3FIFOEnabled(false);
+
+    _accelgyro.setAccelFIFOEnabled(true);
+    _accelgyro.setXGyroFIFOEnabled(true);
+    _accelgyro.setYGyroFIFOEnabled(true);
+    _accelgyro.setZGyroFIFOEnabled(true);
+
+    _accelgyro.setInterruptMode(false);
+    _accelgyro.setFIFOEnabled(true);
+
+    // clear interrupt flags
+    _accelgyro.getIntStatus();
+
+#endif
+
     _set_filter_frequency(_mpu6000_filter);
 
     i2c_sem->give();
@@ -87,7 +113,78 @@ uint16_t AP_InertialSensor_MPU60X0::_init_sensor( Sample_rate sample_rate )
 	return 0;
 }
 
+#ifdef MPU60X0_ENABLE_FIFO
 
+void AP_InertialSensor_MPU60X0::_accumulate(void){
+
+	// get pointer to i2c bus semaphore
+	AP_HAL::Semaphore* i2c_sem = hal.i2c->get_semaphore();
+
+	// take i2c bus semaphore
+	if (!i2c_sem->take_nonblocking()){
+		return;
+	}
+
+    int i = 0, index = 0;
+	
+    int16_t ax, ay, az;
+	int16_t gx, gy, gz;
+    
+    const int packetSize = 12;
+    uint8_t data[packetSize];
+
+    uint16_t fifoCount = _accelgyro.getFIFOCount();
+    uint8_t status = _accelgyro.getIntStatus();
+    
+    if (true && ((status & 0x10) || fifoCount == 1024)) {
+        _accelgyro.resetFIFO();
+        hal.console->printf("FIFO overflown! %d bytes\n", fifoCount);
+        i2c_sem->give();
+        return;
+    }
+    else if (status & 0x01) {
+        int remainingBytes = fifoCount;
+        while (remainingBytes > packetSize)
+        {
+            _accelgyro.getFIFOBytes(data, packetSize);
+            i++;
+            //hal.console->printf("got %d samples\n", fifoCount);
+
+            index = 0;
+            ax = (data[index+0] << 8) | data[index+1];
+            index += 2;
+            ay = (data[index+0] << 8) | data[index+1];
+            index += 2;
+            az = (data[index+0] << 8) | data[index+1];
+            index += 2;
+            gx = (data[index+0] << 8) | data[index+1];
+            index += 2;
+            gy = (data[index+0] << 8) | data[index+1];
+            index += 2;
+            gz = (data[index+0] << 8) | data[index+1];
+
+            remainingBytes -= packetSize;
+        
+            //hal.console->printf("a/g: %d %6hd %6hd %6hd   %6hd %6hd %6hd\n",i, ax,ay,az,gx,gy,gz);
+	
+            _accel_filtered = Vector3f(
+                _accel_filter_x.apply(ax),
+                _accel_filter_y.apply(ay),
+                _accel_filter_z.apply(az));
+
+            _gyro_filtered = Vector3f(
+                _gyro_filter_x.apply(gx),
+                _gyro_filter_y.apply(gy),
+                _gyro_filter_z.apply(gz));
+
+            _gyro_samples_available++;
+       }
+    }
+    i2c_sem->give();
+
+}
+
+#else
 void AP_InertialSensor_MPU60X0::_accumulate(void){
 
 	// get pointer to i2c bus semaphore
@@ -120,6 +217,7 @@ void AP_InertialSensor_MPU60X0::_accumulate(void){
 	i2c_sem->give();
 
 }
+#endif
 
 bool AP_InertialSensor_MPU60X0::_sample_available(void)
 {
